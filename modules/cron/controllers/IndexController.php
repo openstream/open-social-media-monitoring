832dcfd2565e0eee1450ecee98d407c9726d3743
Tim Bezhashvyly <tim@openstream.ch>
2012-09-04 14:27:30 +0200

    OSMM-5: module(s) config is also changed into XML

--- modules/cron/controllers/IndexController.php
+++ modules/cron/controllers/IndexController.php
@@ -5,7 +5,7 @@ class Cron_IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         $bootstrap = $this->getInvokeArg('bootstrap');
-        $cron = $bootstrap->getOption('cron.model');
+        $cron = $bootstrap->getOption('cron');
         if (is_array($cron)) {
             foreach ($cron as $model) {
                 if (class_exists($model)) {
--- modules/default/Bootstrap.php
+++ modules/default/Bootstrap.php
@@ -3,7 +3,7 @@ class Default_Bootstrap extends Zend_Application_Module_Bootstrap
 {
     protected function _initModuleConfig()
     {
-        $iniOptions = new Zend_Config_Ini(dirname(__FILE__) . '/configs/application.ini');
+        $iniOptions = new Zend_Config_Xml(dirname(__FILE__) . '/configs/application.xml');
         $this->getApplication()->setOptions($iniOptions->toArray());
     }
 }
