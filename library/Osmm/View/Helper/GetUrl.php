deeca91bfa0d275b84446c30fa5e9f151276aaab
Tim Bezhashvyly <tim@openstream.ch>
2012-09-05 11:19:05 +0200

    OSMM-11: GetUrl moved to global helpers

--- library/Osmm/View/Helper/GetUrl.php
+++ library/Osmm/View/Helper/GetUrl.php
@@ -1,19 +1,20 @@
 <?php
 
-class Default_View_Helper_GetUrl extends Zend_View_Helper_BaseUrl
+class Osmm_View_Helper_GetUrl extends Zend_View_Helper_BaseUrl
 {
     public function getUrl($route)
     {
         $route = explode('/', $route);
         $route = array_filter($route);
+        $request = Zend_Controller_Front::getInstance()->getRequest();
         if ($route[0] == '*') {
-            $route[0] = Zend_Controller_Front::getInstance()->getRequest()->getModuleName();
+            $route[0] = $request->getModuleName();
         }
         if ($route[1] == '*') {
-            $route[1] = Zend_Controller_Front::getInstance()->getRequest()->getControllerName();
+            $route[1] = $request->getControllerName();
         }
         if ($route[2] == '*') {
-            $route[2] = Zend_Controller_Front::getInstance()->getRequest()->getActionName();
+            $route[2] = $request->getActionName();
         }
         $url = implode('/', $route) . '/';
         $url = (preg_match('/https/i', $_SERVER['SERVER_PROTOCOL']) ? 'https://' : 'http://').$_SERVER['SERVER_NAME'].$this->baseUrl($url);
