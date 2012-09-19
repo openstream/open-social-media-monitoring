26c0419ddf4185ea9c03ff464581061894da1da7
Tim Bezhashvyly <tim@openstream.ch>
2012-09-19 14:57:49 +0200

    OSMM-11: Settings application.xml added.

--- modules/settings/Bootstrap.php
+++ modules/settings/Bootstrap.php
@@ -1,4 +1,10 @@
 <?php
+
 class Settings_Bootstrap extends Zend_Application_Module_Bootstrap
 {
+    protected function _initModuleConfig()
+    {
+        $iniOptions = new Zend_Config_Xml(dirname(__FILE__) . '/configs/application.xml');
+        $this->getApplication()->setOptions($iniOptions->toArray());
+    }
 }
